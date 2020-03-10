f74c5ea52ec6f5f23fac8d9158f6f6b83023bb4f
Gael <gael.henry.sae@gmail.com>
2020-03-10 10:48:51 +0100

    add file forget

---
+++ Assets/Scripts/World/Skid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skid : MonoBehaviour
+{
+    [SerializeField] bool oil;
+    [SerializeField] bool barrel;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<PlayerController>().IsSkid();
+        }
+    }
+}
