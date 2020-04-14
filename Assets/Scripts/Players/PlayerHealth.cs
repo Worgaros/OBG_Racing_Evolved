adddee77902f81c2d934917bfd763a674c5cc6d7
Gael <gael.henry.sae@gmail.com>
2020-04-14 17:21:06 +0200

    add UI Health

--- Assets/Scripts/Players/PlayerHealth.cs
+++ Assets/Scripts/Players/PlayerHealth.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int Health;
+    [SerializeField] int maxHealth;
 
     [SerializeField] bool armoredUp = false;
     int armor = 2;
     
     CoinsSaver coinsave;
+
+    public Image[] heart;
+    public Sprite fullHeart;
+    public Sprite emptyHealth;
 
     const int armorPrice = 15;
 
@@ -28,6 +35,31 @@ public class PlayerHealth : MonoBehaviour
         if (Health == 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Health > maxHealth)
+        {
+            Health = maxHealth;
+        }
+
+        for (int i = 0; i < heart.Length; i++)
+        {
+            if (i < Health)
+            {
+                heart[i].sprite = fullHeart;
+            }
+            else
+            {
+                heart[i].sprite = emptyHealth;
+            }
+            if (i < maxHealth)
+            {
+                heart[i].enabled = true;
+            }
+            else
+            {
+                heart[i].enabled = false;
+            }
         }
     }
     public void Hiting()
